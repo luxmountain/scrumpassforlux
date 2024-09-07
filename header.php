2d009e5e7707b3970332be510ef72164d8e581a5
“lux” <tacaoson230804@gmail.com>
2024-09-07 11:26:03 +0700

    Page Template File

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Blog Site Template">
     <meta name="author" content="https://youtube.com/FollowAndrew">    
-    <link rel="shortcut icon" href="images/logo.png"> 
+    <link rel="shortcut icon" href="/wp-content/themes/scrumforpredev/assets/images/logo.png"> 
     
     <?php 
     wp_head(  );
@@ -27,8 +27,14 @@
 			<span class="navbar-toggler-icon"></span>
 			</button>
 
+            <?php 
+            if(function_exists('the_custom_logo')){
+                $custom_logo_id = get_theme_mod('custom_logo');
+                $logo = wp_get_attachment_image_src($custom_logo_id);
+            }
+            ?>
 			<div id="navigation" class="collapse navbar-collapse flex-column" >
-				<img class="mb-3 mx-auto logo" src="images/logo.png" alt="logo" >			
+				<img class="mb-3 mx-auto logo" src="<?php echo $logo[0] ?>" alt="logo" >			
 				
                 <?php 
                     wp_nav_menu(
@@ -53,3 +59,9 @@
 			</div>
 		</nav>
     </header>
+    <div class="main-wrapper">
+	    <header class="page-title theme-bg-light text-center gradient py-5">
+			<h1 class="heading">
+                <?php the_title();?>
+            </h1>
+		</header>
